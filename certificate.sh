4c4097c6a47c573c4e313741604fd96c52acc2d6
chioma <chiedozieokoye@gmail.com>
2022-11-22 18:28:09 +0000

    certified sure

---
+++ certificate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#certificate will be ready in FEB 2023  by the special grace of God
--- create.sh
+++ create.sh
@@ -4,3 +4,7 @@
 #its quiet intresting 
 #and tasking as well
 #i am grateful for the opportunity to be in class 30
+#we will victorious in Jesus name
+#God will help us finish well
+#
+
