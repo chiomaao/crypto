18daf87c4729d79e7c08df225e1122bdaa3de98f
chioma <chiedozieokoye@gmail.com>
2022-11-22 17:35:41 +0000

    joyful

---
+++ made.java
@@ -0,0 +1,8 @@
+#!/bin/bash
+#gb nation is ecosystem we created as a platform based on web 3.0
+our vision we can create the ecosystem and community which bring benefit that socienty needs
+our mission
+bridging digital company and conventional company through community based on blockchain technology
+
+#so what are you waiting for ?
+there is something big happens in rgb! lets join us
---
+++ visa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#To join the private club you should make a few steps☺️
+Let’s start from registration with a broker.
+
+#Register on Pocket Option by the link below.
+(Previously clean the cookie-files in your browser.)
+IT’S VERY IMPORTANT!
